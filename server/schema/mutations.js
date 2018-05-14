9822042bc373a4f78ca998291043164dc3f06345
cmcaboy <cory.mcaboy@gmail.com>
2018-05-14 08:21:37 -0400

    first commit

---
+++ server/schema/mutations.js
@@ -0,0 +1,37 @@
+const graphql = require('graphql');
+const {
+  GraphQLObjectType,
+  GraphQLString
+} = graphql;
+
+const UserType = require('./types/user_type');
+const AuthService = require('../services/auth');
+
+const mutation = new GraphQLObjectType({
+  name: 'Mutation',
+  fields: {
+    signup: {
+      type: UserType,
+      args: {
+        email: { type: GraphQLString },
+        password: { type: GraphQLString }
+      },
+      // resolve offers a 3rd argument: request
+      // This represents the request object coming from express
+      resolve(parentValue, { email, password }, req) {
+        return AuthService.signup({ email, password, req })
+      }
+    },
+    logout: {
+      type: UserType, 
+      resolve(parentValue, args, req) {
+        // we have to save a ref to the user because we won't get access to it after we logout.
+        const { user } = req; 
+        req.logout();
+        return user;
+      }
+    }
+  }
+})
+
+module.exports = mutation;
